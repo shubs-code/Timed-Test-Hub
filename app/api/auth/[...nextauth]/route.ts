4ac4eb808badeb7f0a7a5f9b116e4a47afe39196
shubs-code <shubs.code@gmail.com>
2024-12-15 23:54:07 +0530

    fixes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,69 +1,8 @@
+/* eslint-disable  */
+
 import NextAuth from 'next-auth'
-import { NextAuthOptions } from "next-auth";
-import GoogleProvider from 'next-auth/providers/google'
+import { authOptions } from '@/lib/auth';
 
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
-
-import { db } from "@/lib/db";
-
-
-const prisma = new PrismaClient()
-
-
-const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  session: {
-    strategy: "jwt",
-  },
-  providers: [
-    // OAuth authentication providers...
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID ?? "",
-      clientSecret: process.env.GOOGLE_SECRET ?? ""
-    }),
-    // Passwordless / email sign in
-  ],
-  callbacks: {
-    async session({ token, session }) {
-      if (token) {
-        //@ts-ignore
-        session.user.id = token.id ;
-        //@ts-ignore
-        session.user.name = token.name;
-        //@ts-ignore
-        session.user.email = token.email;
-        //@ts-ignore
-        session.user.image = token.picture;
-      }
-      return session;
-    },
-    async jwt({ token, user }) {
-
-      const dbUser = await db.user.findFirst({
-        where: {
-          email: token.email,
-        },
-      });
-
-      if (!dbUser) {
-        if (user) {
-          token.id = user?.id;
-        }
-        return token;
-      }
-
-      return {
-        id: dbUser.id,
-        name: dbUser.name,
-        email: dbUser.email,
-        picture: dbUser.image,
-      };
-    },
-  },
-  
-}
-
-const handler = NextAuth(authOptions);
+const handler = (req: Request, res: Response) => NextAuth(req as any, res as any, authOptions);
 
 export { handler as GET, handler as POST };
---
+++ lib/auth.ts
@@ -0,0 +1,63 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from 'next-auth/providers/google'
+
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+import { db } from "@/lib/db";
+
+const prisma = new PrismaClient()
+
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    // OAuth authentication providers...
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID ?? "",
+      clientSecret: process.env.GOOGLE_SECRET ?? ""
+    }),
+    // Passwordless / email sign in
+  ],
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        //@ts-ignore
+        session.user.id = token.id ;
+        //@ts-ignore
+        session.user.name = token.name;
+        //@ts-ignore
+        session.user.email = token.email;
+        //@ts-ignore
+        session.user.image = token.picture;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+
+      const dbUser = await db.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      });
+
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id;
+        }
+        return token;
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      };
+    },
+  },
+  
+}
